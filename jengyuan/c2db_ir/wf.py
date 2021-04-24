65187b2fad9bc9a8fe3978202fbcc0baebaf73dc
jeng-yuantsai <tsaie79@gmail.com>
2021-04-24 00:30:12 -0400

    c2db_ir update

--- jengyuan/c2db_ir/wf.py
+++ jengyuan/c2db_ir/wf.py
@@ -21,7 +21,7 @@ from pytopomat.workflows.fireworks import IrvspFW
 from mpinterfaces.utils import ensure_vacuum
 
 c2db = VaspCalcDb.from_db_file("/home/tug03990/scripts/read-IR/jengyuan/c2db_ir/c2db.json")
-for e in list(c2db.collection.find({"magstate":"NM"}))[1:2]:
+for e in list(c2db.collection.find({"magstate":"NM"}))[2:3]:
     st = e["structure"]
 
     os.makedirs("symmetrized_st", exist_ok=True)
@@ -46,13 +46,14 @@ for e in list(c2db.collection.find({"magstate":"NM"}))[1:2]:
     wf = Workflow(fws, name=wf.name)
 
     lpad = LaunchPad.from_file(os.path.expanduser(
-        os.path.join("~", "config/project/C2DB_IR/calc_data/my_launchpad.yaml")))
+        os.path.join("~", "config/project/testIR/irvsp_test/my_launchpad.yaml")))
     wf = clean_up_files(wf, ("WAVECAR*", "CHGCAR*"), wf.fws[-1].name, task_name_constraint=wf.fws[-1].tasks[-1].fw_name)
     uis_encut = MPRelaxSet(st).incar.get("ENCUT", None)*1.3
     wf = add_modify_incar(wf, {"incar_update": {"ENCUT": uis_encut}})
     wf = add_additional_fields_to_taskdocs(wf, {"c2db_uid": e["uid"]})
-    wf = set_execution_options(wf, category="calc_data")
+    wf = set_execution_options(wf, category="irvsp_test")
     wf = preserve_fworker(wf)
+    wf = set_queue_options(wf, walltime="00:30:00", fw_name_constraint=wf.fws[-1].name)
     wf.name = wf.name + ":{}".format(e["uid"])
     lpad.add_wf(wf)
     print(wf)
